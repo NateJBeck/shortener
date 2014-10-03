49b8a6a13fee21ee7572c445e4c301338fa65a7e
Nathan Beck <nathan.beck10@gmail.com>
2014-10-03 15:06:19 -0400

    The enter url redirects to root

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -8,6 +8,19 @@ class UrlsController < ApplicationController
 
   def create
     @url = Url.new
+    @new_url = shorten_url
+
+    redirect_to "/"
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:original)
+  end
+
+  def shorten_url
+    @url 
   end
 
 end
